76266975ece68028afeb5c04ebe87860d4f4363e
Aziz <shaikaziz2go@gmail.com>
2025-09-23 21:46:22 -0400

    Updated file

--- test.py
+++
@@ -1,5 +0,0 @@
-import google.generativeai as genai
-genai.configure(api_key="AIzaSyDsndhQ3rucYWzivfdZWHsmQFAPN4D1CBI")
-for model in genai.list_models():
-    if 'generateContent' in model.supported_generation_methods:
-        print(f"Supported Model: {model.name}")
